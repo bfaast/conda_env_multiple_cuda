7e1c14d1047af7bb30f991d8e70daa9e533a9b22
bfaast <phz218440@iitd.ac.in>
2023-09-04 10:44:47 +0530

    Initial commit with activate.d/ and deactivate.d/

---
+++ activate.d/env_vars.bat
@@ -0,0 +1,4 @@
+@echo off
+set ORIGINAL_PATH=%PATH%
+set NEW_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\libnvvp
+set "PATH=%NEW_PATH%;%PATH%"
---
+++ deactivate.d/env_vars.bat
@@ -0,0 +1,3 @@
+@echo off
+
+set "PATH=%ORIGINAL_PATH%"
